b21303f535460ddcf363ad967cf683e70ac37d6f
Brinkslu991 <153300807+Brinkslu991@users.noreply.github.com>
2025-02-27 11:24:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -30,6 +30,8 @@ def main():
         # Limit the frame rate to the specified frames per second (FPS)
         clock.tick(config.FPS) # Use the clock to control the frame rate
 
+        print(PLEASE STOP USING ME)
+
     pygame.quit()
 
     sys.exit()
